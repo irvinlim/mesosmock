1a1ff51a0a62e16f505b3416a9f76a5d7c2b6ffc
Irvin Lim <limir@seagroup.com>
2018-10-24 11:20:24 +0800

    Refactor scheduler to handle all errors the same way

--- scheduler.go
+++ scheduler.go
@@ -12,7 +12,7 @@ import (
 	"github.com/mesos/mesos-go/api/v1/lib/scheduler"
 )
 
-// Scheduler returns a Handler for providing the Mesos Scheduler HTTP API:
+// Scheduler returns a http.Handler for providing the Mesos Scheduler HTTP API:
 // https://mesos.apache.org/documentation/latest/scheduler-http-api/
 func Scheduler(opts *Options) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -24,29 +24,34 @@ func Scheduler(opts *Options) http.Handler {
 			return
 		}
 
-		callTypeHandlers := map[scheduler.Call_Type]func(*scheduler.Call, http.ResponseWriter){
-			scheduler.Call_SUBSCRIBE: subscribe,
-		}
-
-		if call.Type == scheduler.Call_UNKNOWN {
+		err = callMux(call, w)
+		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprint(w, "Failed to validate scheduler::Call: Expecting 'type' to be present")
+			fmt.Fprintf(w, "Failed to validate scheduler::Call: %#v", err)
 			return
 		}
-
-		// Invoke handler for different call types
-		handler := callTypeHandlers[call.Type]
-		if handler == nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprint(w, "Failed to validate scheduler::Call: Handler not implemented")
-			return
-		}
-
-		handler(call, w)
 	})
 }
 
-func subscribe(call *scheduler.Call, w http.ResponseWriter) {
+func callMux(call *scheduler.Call, w http.ResponseWriter) error {
+	callTypeHandlers := map[scheduler.Call_Type]func(*scheduler.Call, http.ResponseWriter) error{
+		scheduler.Call_SUBSCRIBE: subscribe,
+	}
+
+	if call.Type == scheduler.Call_UNKNOWN {
+		return fmt.Errorf("Expecting 'type' to be present")
+	}
+
+	// Invoke handler for different call types
+	handler := callTypeHandlers[call.Type]
+	if handler == nil {
+		return fmt.Errorf("Handler not implemented")
+	}
+
+	return handler(call, w)
+}
+
+func subscribe(call *scheduler.Call, w http.ResponseWriter) error {
 	flusher, ok := w.(http.Flusher)
 	if !ok {
 		panic("expected http.ResponseWriter to be an http.Flusher")
@@ -56,7 +61,6 @@ func subscribe(call *scheduler.Call, w http.ResponseWriter) {
 	streamID, err := uuid.NewUUID()
 	if err != nil {
 		log.Panicf("Cannot create stream ID for SUBSCRIBE call: %#v", err)
-		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -67,7 +71,7 @@ func subscribe(call *scheduler.Call, w http.ResponseWriter) {
 	event := &scheduler.Event{
 		Type: scheduler.Event_SUBSCRIBED,
 		Subscribed: &scheduler.Event_Subscribed{
-			FrameworkID:              call.FrameworkID,
+			FrameworkID:              call.Subscribe.FrameworkInfo.ID,
 			HeartbeatIntervalSeconds: &heartbeat,
 		},
 	}
@@ -75,7 +79,6 @@ func subscribe(call *scheduler.Call, w http.ResponseWriter) {
 	res, err := event.MarshalJSON()
 	if err != nil {
 		log.Panicf("Cannot marshal JSON for SUBSCRIBE call: %#v", err)
-		return
 	}
 
 	for {
